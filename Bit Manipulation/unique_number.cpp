d5ccfd0269cd9f10c1e7922b01a15412fa030655
Xanjit Xarkar <ssanjit856@gmail.com>
2020-10-27 21:01:06 +0530

    Added Find Unique Number in an Array Program

--- Bit Manipulation/unique_number.cpp
+++ Bit Manipulation/unique_number.cpp
@@ -1,20 +1,46 @@
-// C program to get toggle case of a string 
-#include <iostream> 
-void *changeLetterCase(char *a) 
-{ 
-    for (int i=0; a[i]!='\0'; i++) { 
-  
-        a[i] ^= 32; 
-    } 
-  
-    cout<<"The Altered Letter Case String: "<<a<<endl;
-} 
-  
-// Driver Code 
-int main() 
-{ 
-    char str[100];
-    cin>>str;
-   cout<<"The Original String: "<<str<<endl;
-   changeLetterCase(str);
-} 
+//Program to find unique number in an array
+#include <iostream>
+using namespace std;
+
+int unique(int *arr,int n)
+{
+	int count[64]={0};
+
+	for(int k=0;k<n;k++)
+	{
+		int i=0;
+		int num=arr[k];
+		while(num>0)
+		{
+			count[i]+=(num&1);
+			i++;
+			num=num>>1;
+		}
+	}
+	int power=1;
+	int result=0;
+	for(int j=0;j<64;j++)
+	{
+		count[j] %=3;
+		result+=count[j]*power;
+		power=power<<1;
+	}
+	return result;
+}
+
+int main()
+{
+	int arr[50];
+	int n;
+	cout<<"Enter length of the array: ";
+	cin>>n;
+
+	cout<<"Enter array elements: "<<endl;
+	for(int c=0;c<n;c++)
+	{
+		cin>>arr[c];
+	}
+	cout<<unique(arr,n)<<" is the unique number in array.";
+
+	return 0;
+}
